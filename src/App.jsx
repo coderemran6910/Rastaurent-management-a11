a878cd0f2b2661ecb427b51869b420df4f344162
Coder Emran <coderemran6910@gmail.com>
2023-12-13 12:40:00 +0600

    added messenger feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import Footer from "./components/Footer"
 import MainLayout from "./layout components/mainLayout"
 import {Outlet} from "react-router-dom"
 
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
 
   return (
     <>
+    <MessengerCustomerChat
+    pageId="107100258277950"
+    appId="209877768834462"
+  />
+
+
       <MainLayout>
         <Outlet></Outlet>
         <Footer></Footer>
